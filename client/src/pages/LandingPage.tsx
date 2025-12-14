f0b28f6e9c7711c408e578fa9775ccfbb1360ba4
iLordMaster <ayoubthki@gmail.com>
2025-12-14 04:20:04 +0100

    companion

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
 import { Sparkles, Heart, Brain, Shield } from "lucide-react";
+import HeroImage from "../assets/LDR_LLL1_0.png";
 
 export default function LandingPage() {
     const navigate = useNavigate();
