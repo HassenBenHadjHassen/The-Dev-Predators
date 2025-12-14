bbaa5f336b7aa6f6a59e63329b1786733efcb8ee
iLordMaster <ayoubthki@gmail.com>
2025-12-14 07:16:13 +0100

    t

--- client/src/api/timelineApi.ts
+++ client/src/api/timelineApi.ts
@@ -1,18 +1,25 @@
 import Api from "./api";
 
 export class TimelineApi extends Api {
-    static api_url = Api.api_url + "/timeline";
+  static get api_url() {
+    return Api.api_url + "/timeline";
+  }
 
-    static async getTimeline() {
-        return this.request(this.api_url, {
-            method: "GET",
-        });
-    }
+  static async getTimeline() {
+    return this.request(this.api_url, {
+      method: "GET",
+    });
+  }
 
-    static async createEvent(data: { title: string; type: string; description?: string; stressChange?: number }) {
-        return this.request(this.api_url + "/create", {
-            method: "POST",
-            body: JSON.stringify(data),
-        });
-    }
+  static async createEvent(data: {
+    title: string;
+    type: string;
+    description?: string;
+    stressChange?: number;
+  }) {
+    return this.request(this.api_url + "/create", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  }
 }
