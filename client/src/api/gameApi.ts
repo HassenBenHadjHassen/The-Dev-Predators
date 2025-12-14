afcad05cba697b1bf69bfa14c1579d371cd034b5
iLordMaster <ayoubthki@gmail.com>
2025-12-14 07:15:49 +0100

    test 2

--- client/src/api/gameApi.ts
+++ client/src/api/gameApi.ts
@@ -1,12 +1,14 @@
 import Api from "./api";
 
 export class GameApi extends Api {
-    static api_url = Api.api_url + "/game";
+  static get api_url() {
+    return Api.api_url + "/game";
+  }
 
-    static async recordEvent(data: { type: "WIN" | "LOSS"; level: number }) {
-        return this.request(`${this.api_url}/event`, {
-            method: "POST",
-            body: JSON.stringify(data),
-        });
-    }
+  static async recordEvent(data: { type: "WIN" | "LOSS"; level: number }) {
+    return this.request(`${this.api_url}/event`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+  }
 }
